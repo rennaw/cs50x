a15880c2a4fa16ab6e11a8db4ba2a2d751a96d65
Sam Hatfield <samuel.e.hatfield@gmail.com>
2014-03-07 17:34:12 -0600

    Wrote out the framework for recover, now I just have to get it to work. So far, I get a seg fault.

--- pset5/jpg/recover.c
+++ pset5/jpg/recover.c
@@ -7,7 +7,101 @@
  * Recovers JPEGs from a forensic image.
  */
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCK 512
+#define TOTAL_JPGS 50
+
+// prototypes
+FILE* newJPG(int num, FILE* ptr); // closes old file if there is one
+bool checkJPGsig(char* buffer); // checks to see if the block has JPEG signatures
+
 int main(int argc, char* argv[])
 {
-    // TODO
+    // VALIDATION
+
+    if (argc != 1)
+    {
+        printf("Usage: recover does not take any arguments\n");
+        printf("File card.raw must be in this directory.");
+        return 1;
+    }
+    // INITIALIZING
+
+    // buffer is char because that will look at hexidecimal pairs
+    char* buffer = malloc(BLOCK);
+    
+    // file pointers
+    FILE* raw = fopen("card.raw", "r");
+    FILE* jpg = NULL;
+
+    // keeping track of recovered files for naming purposes
+    int numJPGs = 0;
+
+    // keeping bool flag for EOF
+    bool eof = false;
+
+    // PROCESSING
+    do
+    {
+        // read the raw file
+        if ( fread(&buffer, BLOCK, 1, raw) == 1)
+        {
+
+            // if JPG signatures are in the block
+            if (checkJPGsig(buffer))
+            {
+                numJPGs++;
+                newJPG(numJPGs, jpg);
+                fwrite(&buffer, BLOCK, 1, jpg);
+            }
+
+            // if a file has been opened for jpg
+            else if (jpg != NULL)
+            {
+                fwrite(&buffer, BLOCK, 1, jpg);
+            }
+
+            // skip the empty block
+            else {fseek(raw, BLOCK, SEEK_CUR);}
+        }
+        // if fread was not able to return 1, we are at eof
+        else {eof = true;}
+
+    } while (!eof && numJPGs < TOTAL_JPGS);
+
+    // finishing
+    free(buffer);
+    fclose(raw);
+    fclose(jpg);
+    return 0;
+}
+
+FILE* newJPG(int num, FILE* ptr)
+{
+    // if there was a file open already
+    if (ptr != NULL) {fclose(ptr);}
+    
+    // create file with sprintf
+    char title[7];
+    sprintf(title, "%3d.jpg", num);
+    ptr = fopen(title, "a");
+    return ptr;
+}
+
+bool checkJPGsig(char* bfr)
+{
+    // eq is short for equal, sig short for JPEG signature
+    bool match = false;
+    char sig1[4] = {0xff, 0xd8, 0xff, 0xe0};
+    char sig2[4] = {0xff, 0xd8, 0xff, 0xe1};
+    for (int i = 0; i < 4; i++)
+    {
+        // if buffer[i] and sig1[i] or sig2[i] match, then they are equal
+        (bfr[i] == sig1[i] || bfr[i] == sig2[i]) ? (match = true) : (match = false);
+    }
+
+    return match;
 }
